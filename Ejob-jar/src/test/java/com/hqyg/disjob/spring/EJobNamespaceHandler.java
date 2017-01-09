0777f26ba66b5803c4f4738e610b6487c7a54b70
huangyiming <huangyiminghappy@163.com>
2017-01-09 15:33:45 +0800

    version

--- Ejob-jar/src/test/java/com/hqyg/disjob/spring/EJobNamespaceHandler.java
+++ Ejob-jar/src/test/java/com/hqyg/disjob/spring/EJobNamespaceHandler.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:Ejob-jar/src/test/java/com/hqyg/disjob/spring/EJobNamespaceHandler.java
 package com.hqyg.disjob.spring;
 /*package com.hqyg.disjob.spring;
 
@@ -12,4 +13,20 @@ public class EJobNamespaceHandler extends NamespaceHandlerSupport{
 		registerBeanDefinitionParser("executor", new EJobBeanDefinitionParser(ExecutorInfo.class, true));
 	}
 }
+=======
+package com.hqyg.disjob.spring;
+/*package com.hqyg.disjob.spring;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+import com.hqyg.disjob.java.bean.JobInfo;
+
+public class EJobNamespaceHandler extends NamespaceHandlerSupport{
+
+	public void init() {
+		registerBeanDefinitionParser("publish", new EJobBeanDefinitionParser(JobInfo.class,true));
+		registerBeanDefinitionParser("executor", new EJobBeanDefinitionParser(ExecutorInfo.class, true));
+	}
+}
+>>>>>>> 31756b5772b8cccc509feda8167e5dd74f23142f:Ejob-jar/src/test/java/com/hqyg/disjob/spring/EJobNamespaceHandler.java
 */
