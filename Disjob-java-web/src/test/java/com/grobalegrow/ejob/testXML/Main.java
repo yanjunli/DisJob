47569e927dbd74272e1e5f8ef67d31a4116d96c7
huangyiming <huangyiminghappy@163.com>
2017-01-15 21:17:21 +0800

    test

--- Disjob-java-web/src/test/java/com/grobalegrow/ejob/testXML/Main.java
+++ Disjob-java-web/src/test/java/com/grobalegrow/ejob/testXML/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"ejob.xml"});
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"disjob.xml"});
 		context.start();
 		System.in.read();
 	}
