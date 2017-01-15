36f90d308e0830164eb8c0c624cf56f82b5e7e96
huangyiming <huangyiminghappy@163.com>
2017-01-15 21:11:36 +0800

    delete some无效代码

--- Rpc/src/main/java/com/hqyg/disjob/rpc/codec/EjobKillTaskResponse.java
+++ Rpc/src/main/java/com/hqyg/disjob/rpc/codec/EjobKillTaskResponse.java
@@ -4,9 +4,7 @@ import java.io.Serializable;
 
 public class EjobKillTaskResponse implements EjobResponse, Serializable {
 
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 
 	private boolean status;
